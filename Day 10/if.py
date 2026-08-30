2ed2aae56e2b335eff2568855b4bcf84b5553eb0
Umakanth <umakanth1445@gmail.com>
2026-08-30 21:51:52 +0530

    Conditional statements

---
+++ Day 10/if.py
@@ -0,0 +1,15 @@
+'''sales = int(input("Enter the sales: "))
+
+if sales>1000:
+    print("Best seller")
+
+ali_acnt = eval(input("Eligible Account: "))
+ver_sub = eval(input("Verfied Subscription: "))
+
+if ali_acnt and ver_sub:
+    print("Verified badge granted")'''
+
+rain_status = eval(input("Enter the rain status: "))
+
+if rain_status:
+    print("Extra Rain charges applied")
